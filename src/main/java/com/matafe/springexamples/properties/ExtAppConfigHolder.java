0559c9d9b0733488d6c9f14321ce1984dfdcbb0e
Mauricio T. Ferraz <matafe@gmail.com>
2014-09-02 00:22:56 +0200

    adding external (file) config property

---
+++ src/main/java/com/matafe/springexamples/properties/ExtAppConfigHolder.java
@@ -0,0 +1,26 @@
+package com.matafe.springexamples.properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * Config Holder
+ * 
+ * @author Mauricio Tavares Ferraz
+ */
+@Component
+public class ExtAppConfigHolder
+{
+	@Value("${audit}")
+	private String audit;
+
+	/**
+	 * @return the audit
+	 */
+	public String getAudit()
+	{
+		return audit;
+	}
+
+}
--- src/main/java/com/matafe/springexamples/properties/PropertiesConfig.java
+++ src/main/java/com/matafe/springexamples/properties/PropertiesConfig.java
@@ -1,5 +1,11 @@
 package com.matafe.springexamples.properties;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -22,25 +28,56 @@ public class PropertiesConfig
 	public static void main(String[] args)
 	{
 		System.setProperty("app.pwd", "s3cret");
-		
+
+		String extFilePath = createTempPropertyFile();
+		LOGGER.debug("Trying: {}", extFilePath);
+		System.setProperty("config.file.path", extFilePath);
+
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext-properties.xml", PropertiesConfig.class))
 		{
 			AppConfigHolder config = context.getBean(AppConfigHolder.class);
+			ExtAppConfigHolder extConfig = context
+					.getBean(ExtAppConfigHolder.class);
+			PersistenceConfigHolder persistenceConfig = context
+					.getBean(PersistenceConfigHolder.class);
 
 			LOGGER.debug("App Description: {}", config.getAppDescription());
 			LOGGER.debug("App Author: {}", config.getAppAuthor());
 
-			PersistenceConfigHolder persistenceConfig = context
-					.getBean(PersistenceConfigHolder.class);
+			LOGGER.debug("Audit: {}", extConfig.getAudit());
 
 			LOGGER.debug("JDBC Username: {}", persistenceConfig.getUsername());
 
 			Environment env = context.getBean(Environment.class);
-
 			LOGGER.debug("App Pwd: {}", env.getProperty("app.pwd"));
+
 		}
 
 	}
 
+	private static String createTempPropertyFile()
+	{
+		try
+		{
+			String tmpDir = System.getProperty("java.io.tmpdir");
+			Properties properties = new Properties();
+			properties.setProperty("audit", "false");
+			properties.setProperty("now",
+					String.valueOf(System.currentTimeMillis()));
+			File file = new File(tmpDir, "app-config.properties");
+			FileOutputStream fileOut = new FileOutputStream(file);
+			properties.store(fileOut, "Spring Example Ext App Config");
+			fileOut.close();
+			return file.getAbsolutePath();
+		} catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+
+	}
 }
